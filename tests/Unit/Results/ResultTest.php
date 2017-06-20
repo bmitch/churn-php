9a6d22766838544cfe531c16f279595ce1f7b5fd
Bill Mitchell <wkmitch@gmail.com>
2017-06-20 16:54:10 -0700

    Fixes #27

---
+++ tests/Unit/Results/ResultTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types = 1);
+
+namespace Churn\Tests\Assessors\CyclomaticComplexity;
+
+use Churn\Tests\BaseTestCase;
+use Churn\Results\Result;
+
+class ResultTest extends BaseTestCase
+{
+    /**
+     * The object we're testing.
+     * @var Result
+     */
+    protected $result;
+
+    /** @test */
+    public function it_can_be_created()
+    {
+        $this->assertInstanceOf(Result::class, $this->result);
+    }
+
+    /** @test */
+    public function it_can_return_the_file()
+    {
+        $this->assertSame('filename.php', $this->result->getFile());
+    }
+
+    /** @test */
+    public function it_can_return_the_commits()
+    {
+        $this->assertSame(5, $this->result->getCommits());
+    }
+
+    /** @test */
+    public function it_can_return_the_complexity()
+    {
+        $this->assertSame(7, $this->result->getComplexity());
+    }
+
+    /** @test */
+    public function it_can_calculate_the_score()
+    {
+        $this->assertSame(12, $this->result->getScore());
+    }
+
+    public function setup()
+    {
+        $this->result = new Result([
+            'file'       => 'filename.php',
+            'commits'    => 5,
+            'complexity' => 7,
+        ]);
+    }
+}
