d3e175b91ed21e1ca4457ee6e707ef8ece7a865b
Mattias Lyckne <mattias@lyckne.se>
2017-09-29 14:42:46 +0200

    Make it possible to run from global install

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+if (is_file($autoload = __DIR__.'/vendor/autoload.php')) {
+    require_once($autoload);
+} elseif (is_file($autoload = __DIR__ . '/../../autoload.php')) {
+    require_once($autoload);
+} else {
+    fwrite(STDERR,
+        'You must set up the project dependencies first. Run the following command:'.PHP_EOL.
+        'composer install'.PHP_EOL
+    );
+    exit(1);
+}
