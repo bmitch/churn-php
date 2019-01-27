ec92b47c48be93182a63c5f900f68638be603ffd
Andreas Möller <am@localheinz.com>
2019-01-27 22:38:58 +0100

    Fix: Add to assertion count from Mockery

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -2,9 +2,16 @@
 
 namespace Churn\Tests;
 
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 abstract class BaseTestCase extends TestCase
 {
-
+    /**
+     * @see https://github.com/phpspec/prophecy/issues/366#issuecomment-359587114
+     */
+    protected function tearDown()
+    {
+        $this->addToAssertionCount(m::getContainer()->mockery_getExpectationCount());
+    }
 }
