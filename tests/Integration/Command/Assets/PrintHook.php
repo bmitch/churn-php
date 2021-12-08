5e0b4828252c686676881856414fdeec0ae3879c
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-12-08 14:52:23 +0100

    Fix tests

--- tests/Integration/Command/Assets/PrintHook.php
+++ tests/Integration/Command/Assets/PrintHook.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Churn\Tests\Integration\Command\Assets;
 
-use Churn\Event\Event\AfterAnalysisEvent;
-use Churn\Event\Event\BeforeAnalysisEvent;
+use Churn\Event\Event\AfterAnalysis as AfterAnalysisEvent;
+use Churn\Event\Event\BeforeAnalysis as BeforeAnalysisEvent;
 use Churn\Event\Hook\AfterAnalysisHook;
 use Churn\Event\Hook\BeforeAnalysisHook;
 
--- tests/Integration/Command/Assets/TestHook.php
+++ tests/Integration/Command/Assets/TestHook.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Churn\Tests\Integration\Command\Assets;
 
-use Churn\Event\Event\AfterAnalysisEvent;
-use Churn\Event\Event\AfterFileAnalysisEvent;
-use Churn\Event\Event\BeforeAnalysisEvent;
+use Churn\Event\Event\AfterAnalysis as AfterAnalysisEvent;
+use Churn\Event\Event\AfterFileAnalysis as AfterFileAnalysisEvent;
+use Churn\Event\Event\BeforeAnalysis as BeforeAnalysisEvent;
 use Churn\Event\Hook\AfterAnalysisHook;
 use Churn\Event\Hook\AfterFileAnalysisHook;
 use Churn\Event\Hook\BeforeAnalysisHook;
