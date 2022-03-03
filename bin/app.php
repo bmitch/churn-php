59d55f3b8a5c93cae6abd847beb3126af2ea2f6a
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-03-03 13:15:45 +0000

    Apply CS to bin/

--- bin/app.php
+++ bin/app.php
@@ -7,11 +7,11 @@ use Churn\Command\RunCommand;
 use Composer\InstalledVersions;
 use Symfony\Component\Console\Application;
 
-$application = new Application('churn-php', (function (string $package): string {
+$application = new Application('churn-php', (static function (string $package): string {
     $version = InstalledVersions::getPrettyVersion($package);
     $ref = InstalledVersions::getReference($package);
     if ($ref) {
-        $version .= '@' . \substr($ref, 0, 7);
+        $version .= '@' . substr($ref, 0, 7);
     }
 
     return $version;
